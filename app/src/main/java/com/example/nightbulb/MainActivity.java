27bf664c69c79bd654fef906cf4aa6b25c9c576d
unicsam <hello.saurav1@gmail.com>
2023-09-06 07:16:40 +0530

    Basic Night Light

--- app/src/main/java/com/example/nightbulb/MainActivity.java
+++ app/src/main/java/com/example/nightbulb/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.nightbulb;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class MainActivity extends Activity {
     private float initialBrightness = 1.0f; // Initial brightness value
 
+    ProgressBar brightnessProgressBar; // Declare the ProgressBar variable
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        brightnessProgressBar = findViewById(R.id.brightnessProgressBar);
+
 
         final View mainLayout = findViewById(R.id.mainLayout);
 
@@ -29,14 +34,22 @@ public class MainActivity extends Activity {
                         float newBrightness = event.getY() / v.getHeight();
                         if (newBrightness > 1.0f) {
                             newBrightness = 1.0f;
-                        } else if (newBrightness < 0.1f) {
-                            newBrightness = 0.1f;
+                        } else if (newBrightness < 0.0f) {
+                            newBrightness = 0.0f;
                         }
+
+                        // Update the ProgressBar
+                        int progress = (int) (newBrightness * 100); // Scale to 0-100 range
+                        brightnessProgressBar.setProgress(progress);
+
+                        Log.d("Brightness", "Progress: " + progress);
+
 
                         // Apply the new brightness
                         WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
                         layoutParams.screenBrightness = newBrightness;
                         getWindow().setAttributes(layoutParams);
+
                         break;
                 }
                 return true;
